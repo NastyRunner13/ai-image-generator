027c7ef999bb85f0b5d69f0b56fd3a82c3343a86
Prince Gupta <princegupta1586@gmail.com>
2023-01-27 23:40:26 +0530

    Update Dalle Route

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
-import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
